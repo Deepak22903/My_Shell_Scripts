69db71c9be8619f73cd93acb60bad8091c298c27
Deepak22903 <deepakshitole4@gmail.com>
2024-08-07 23:35:50 +0530

    Updated

--- evenOdd.sh
+++ evenOdd.sh
@@ -1,7 +1,6 @@
 
 #!/bin/bash
 
-# Function to count even and odd numbers in a range
 count_even_odd() {
     local start=$1
     local end=$2
@@ -12,7 +11,7 @@ count_even_odd() {
     for (( num=start; num<=end; num++ )); do
         if (( num % 2 == 0 )); then
             (( even_count++ ))
-            echo $num
+            echo -n "$num "
         fi
     done
 
@@ -20,28 +19,24 @@ count_even_odd() {
     for (( num=start; num<=end; num++ )); do
         if (( num % 2 != 0 )); then
             (( odd_count++ ))
-            echo $num
+            echo -n "$num "
         fi
     done
     echo "Number of even numbers: $even_count"
     echo "Number of odd numbers: $odd_count"
 }
 
-# Read range from user
 read -p "Enter the start of the range: " start
 read -p "Enter the end of the range: " end
 
-# Validate input
 if ! [[ "$start" =~ ^-?[0-9]+$ ]] || ! [[ "$end" =~ ^-?[0-9]+$ ]]; then
     echo "Please enter valid integers."
     exit 1
 fi
 
-# Ensure start is less than or equal to end
 if [ $start -gt $end ]; then
     echo "Start of the range should be less than or equal to the end."
     exit 1
 fi
 
-# Count even and odd numbers in the range
 count_even_odd $start $end
