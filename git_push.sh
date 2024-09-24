94563c43468052b3a3ba54e2ac2ed1bc97866c86
Deepak22903 <deepakshitole4@gmail.com>
2024-09-24 16:09:13 +0530

    Updated

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ./spinner.sh
+
 # Update .gitignore for larger files (>=50M), suppress all output
 /home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/check_large_file_for_git_push_after_diff.sh >/dev/null 2>&1
 
@@ -16,16 +17,14 @@ commit_message=${commit_message:-"Updated"}
 # Commit with the provided or default message, suppress output
 git commit -m "$commit_message" >/dev/null 2>&1
 
+# Start the spinner with the message "Pushing to repository"
+start_spinner "Pushing to repository"
 
-# Display pushing message
-echo -n "Pushing"
+# Push to the remote repository in the background and wait for it to finish
+git push origin main >/dev/null 2>&1
 
-# start_spinner()
-
-# Push to the remote repository in the background
-git push origin main >/dev/null 2>&1 &
+# Stop the spinner once the push is complete
+stop_spinner
 
 # Display push success message
 echo -e "\nPush success"
-
-# stop_spinner()
