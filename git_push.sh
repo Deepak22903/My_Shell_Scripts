fba6d73f484b52d5fc37f5762531b6d6e4789019
Deepak22903 <deepakshitole4@gmail.com>
2024-09-25 16:33:01 +0530

    Updated

--- git_push.sh
+++ git_push.sh
@@ -4,8 +4,14 @@ source /home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/spinner.sh
 # Update .gitignore for larger files (>=50M), suppress all output
 /home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/check_large_file_for_git_push_after_diff.sh >/dev/null 2>&1
 
+# Start the spinner for adding changes
+start_spinner "Adding changes..."
+
 # Add all changes, suppress output
 git add . >/dev/null 2>&1
+
+# Stop the spinner once the add is complete
+stop_spinner
 
 # Prompt for commit message
 echo -n "Commit Message: "
