ed0aa155ae2b930d6e801e7567133fd68a537294
Deepak22903 <deepakshitole4@gmail.com>
2025-02-12 11:05:06 +0530

    Updated

--- misc/NetworkManagerConfAddComment.sh
+++ misc/NetworkManagerConfAddComment.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Colors and formatting
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+BOLD='\033[1m'
+NC='\033[0m' # No Color
+
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <file_path> <start_line>"
@@ -18,4 +27,8 @@ fi
 # Use sed to add # to lines after the specified line
 sed -i "${start_line},\$s/^/#/" "$file_path"
 
-echo "Added # to lines after line $start_line in $file_path."
+echo -e "${YELLOW}Added${NC}${GREEN}${BOLD} # ${NC}${YELLOW}to lines after line $start_line in $file_path.${NC}" 
+
+systemctl restart NetworkManager
+
+echo -e "${GREEN}${BOLD}NetworkManager restarted!${NC}"
