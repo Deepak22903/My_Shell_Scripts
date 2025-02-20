d74ef1b88348d71e4796b55ba6cebe0565196c03
Deepak22903 <deepakshitole4@gmail.com>
2025-02-20 17:04:45 +0530

    added config reloadding or hypridle

--- cpp/screen_idle_controller/screen_idle_controller.cpp
+++ cpp/screen_idle_controller/screen_idle_controller.cpp
@@ -3,6 +3,24 @@
 #include <string>
 using namespace std;
 
+void reload_config() {
+  cout << "Reloading config...\n";
+  int r1 = system("pkill hypridle");
+  if (!(r1 == 0)) {
+    cout << "failed in pkill hypridle!\n";
+  }
+  int r2 = system("hypridle > /dev/null &");
+  if (!(r2 == 0)) {
+    cout << "failed in starting hypridle!\n";
+  }
+  int r3 = system("hyprctl reload > /dev/null");
+  if (!(r3 == 0)) {
+    cout << "failed in reloading hyprctl!\n";
+  }
+
+  cout << "success\n";
+}
+
 void check_errors(int ret) {
   if (ret == 0) {
     cout << "success\n";
@@ -23,25 +41,34 @@ int main(int argc, char *argv[]) {
             "--no-sleep : Prevents the system from suspend state\n"
             "--no-screenoff : Prevents the screen from turning off\n"
             "--default : Restores to original state\n";
-  } else if (string(argv[1]) == "--no-sleep") {
-    int ret = system(
-        "cp "
-        "/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/cpp/"
-        "screen_idle_controller/files/sleep.conf ~/.config/hypr/hypridle.conf");
-    check_errors(ret);
-  } else if (string(argv[1]) == "--no-screenoff") {
-    int ret = system("cp "
-                     "/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/"
-                     "cpp/screen_idle_controller/files/sleep_screenoff.conf "
-                     "~/.config/hypr/hypridle.conf");
-    check_errors(ret);
-  } else if (string(argv[1]) == "--default") {
-    int ret = system("cp "
-                     "/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/"
-                     "cpp/screen_idle_controller/files/default.conf "
-                     "~/.config/hypr/hypridle.conf");
-    check_errors(ret);
   } else {
-    cout << "Invalid input!\n Use screen -h for help.\n";
+    cout << "Updating config...\n";
+    if (string(argv[1]) == "--no-sleep") {
+      int ret =
+          system("cp "
+                 "/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/cpp/"
+                 "screen_idle_controller/files/sleep.conf "
+                 "~/.config/hypr/hypridle.conf");
+      check_errors(ret);
+      reload_config();
+    } else if (string(argv[1]) == "--no-screenoff") {
+      int ret =
+          system("cp "
+                 "/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/"
+                 "cpp/screen_idle_controller/files/sleep_screenoff.conf "
+                 "~/.config/hypr/hypridle.conf");
+      check_errors(ret);
+      reload_config();
+    } else if (string(argv[1]) == "--default") {
+      int ret =
+          system("cp "
+                 "/home/deepak/ghq/github.com/Deepak22903/My_Shell_Scripts/"
+                 "cpp/screen_idle_controller/files/default.conf "
+                 "~/.config/hypr/hypridle.conf");
+      check_errors(ret);
+      reload_config();
+    } else {
+      cout << "Invalid option!\n Use screen -h for help.\n";
+    }
   }
 }
