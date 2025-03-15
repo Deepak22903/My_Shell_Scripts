4710474c783285d8f5d618ecee655ddefc8cbd60
Deepak22903 <deepakshitole4@gmail.com>
2025-03-15 10:13:32 +0530

    new script!

---
+++ global/fiftyTenNotify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sleep 3000
+notify-send -u critical "You have studied for 50 minutes!" "Take a break. You deserve it!"
+sleep 600
+notify-send -u critical "Break is over!" "Back to work!"
